500ff742d35bb2fb6d169dc68ee9f8f209c3c562
STUDENT <huaphuc2414@gmail.com>
2020-11-23 19:03:34 +0700

    Bổ sung trang : Giới thiệu + Liên hệ và các liên kết

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php
-
-echo('<h1 style="color:red;">Xin chào tôi là Hứa Vĩnh Phúc</h1>')
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trang chủ</title>
+</head>
+<body>
+    <h1>Trang chủ</h1>
+    <?php
+        echo('<h1 style="color:red;">Xin chào tôi là Hứa Vĩnh Phúc</h1>')
+    ?>
+    <a href="pages/Gioi-thieu.php">Giới thiệu</a>
+    <a href="pages/Lien-he.php">Liên hệ</a>
+</body>
+</html>
